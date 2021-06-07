b4bfe2707f15d2f3d21e4b7b1dd17d00064aed08
bouffon <22919011+bouffon@users.noreply.github.com>
2021-06-06 22:53:38 -0400

    Add files via upload

--- SEG 3125 Lab 3/scripts/groceries.js
+++ SEG 3125 Lab 3/scripts/groceries.js
@@ -134,25 +134,18 @@ function restrictListProducts(prods, restrictions) {
 	for (let j = 0; j < prods.length; j++){
 
 		var addItem = false;
-
-		console.log("Verifying Item: " + prods[j].name);
-
 		for (let i=0; i<restrictions.length; i+=1){
 
 			if((restrictions[i] == "Organic") && (prods[j].organic == true)){
-				console.log("Organic");
 				addItem = true;
 			}
 			else if ((restrictions[i] == "Lactose-Intolerant") && (prods[j].lactoseIntolerant == true)){
-				console.log("Lactose-Intolerant");
 				addItem = true;
 			}
 			else if ((restrictions[i] == "Nut Allergy") && (prods[j].nutAllergy == true)){
-				console.log("Nut Allergy");
 				addItem = true;
 			}
 			else if (restrictions[i] == "None"){
-				console.log("None");
 				addItem = true;
 			}
 			else{
@@ -161,38 +154,11 @@ function restrictListProducts(prods, restrictions) {
 			}
 		}
 		if(addItem == true){
-			console.log("Adding Item!");
 			product_names.push(prods[j].name + " - $" + prods[j].price.toFixed(2));
 		}
 	}
 	return product_names;
 
-	//!(product_names.includes(prods[i].name + " - $" + prods[i].price.toFixed(2))
-
-}
-
-function sortProducts(prods){
-	let prices = [];
-
-	for (let i=0; i<prods.length; i+=1) {
-		var price = prods[i].slice(prods[i].length-4);
-		prices.push(price);
-	}
-
-	prices.sort();
-	//alert("Prices: " + prices);
-
-	for (let h=0; h<prices.length; h+=1) {
-
-		for (let j=0; j<prods.length; j+=1) {
-
-			if(price[h] == products[i].price){
-				prods[i] = products[i].name + " - $" + products[i].price.toFixed(2);
-			}
-		}
-	}
-
-	return prods;
 }
 
 // Calculate the total price of items, with received parameter being a list of products
